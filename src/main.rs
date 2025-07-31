4bfdbed219072a670478173895660f37c022d355
professionalowo <martin.brandl1307@gmail.com>
2025-07-31 12:05:44 +0200

    refactor: remove success print statement from GLFW initialization

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ mod glfw;
 use glfw::window::Window;
 
 fn main() {
-    match glfw::init() {
-        Ok(_) => println!("GLFW initialized successfully!"),
-        Err(e) => panic!("Failed to initialize GLFW: {:?}", e),
+    if let Err(e) = glfw::init() {
+        panic!("Failed to initialize GLFW: {:?}", e);
     }
     let window = match Window::try_new(640, 320, "Rust") {
         Ok(w) => w,
