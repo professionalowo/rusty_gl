827d940434ad9c4264296fa32279728d8446f0c7
professionalowo <martin.brandl1307@gmail.com>
2025-08-01 18:34:10 +0200

    refactor: clean up imports in window.rs

--- src/glfw/window.rs
+++ src/glfw/window.rs
@@ -1,6 +1,6 @@
 use std::ffi::{NulError, c_int, CString};
 
-use crate::{gl, glfw::GLFWError};
+use crate::glfw::GLFWError;
 
 unsafe extern "C" {
     unsafe fn glfwGetPrimaryMonitor() -> GLFWmonitor;
