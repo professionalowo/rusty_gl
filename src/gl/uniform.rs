5c28e4ff112b24e34a63819cb5cbc464fbdf863d
professionalowo <martin.brandl1307@gmail.com>
2025-08-04 11:19:18 +0200

    refactor: update get_location function parameter type for better flexibility

--- src/gl/uniform.rs
+++ src/gl/uniform.rs
@@ -6,12 +6,13 @@ use crate::{
 pub fn mat3f(location: i32, transpose: bool, matrix: Mat3<f32>) {
     let cols = matrix.cols();
     let value = cols.as_ptr() as *const f32;
-    println!("{:?}", cols);
     unsafe {
         glUniformMatrix3fv(location, 1, u8::from(transpose), value);
     }
 }
 
-pub fn get_location(program: u32, name: &str) -> i32 {
+//TODO: always seems to return -1, even if the uniform exists
+pub fn get_location(program: u32, name: impl AsRef<str>) -> i32 {
+    let name = name.as_ref();
     unsafe { glGetUniformLocation(program, name.as_ptr() as *const i8) }
 }
