fcb1a41d7622110cac1627b1bde067d2e4021275
professionalowo <martin.brandl1307@gmail.com>
2025-08-19 18:21:31 +0200

    fix: correct texture file name from "solid_reddd.png" to "solid_red.png"

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ fn main() {
         .expect("Failed to create shader program");
 
     let texture = Texture2D::try_from_file(
-        get_texture_file_path("solid_reddd.png")
+        get_texture_file_path("solid_red.png")
             .to_str()
             .expect("Invalid texture path"),
         false,
