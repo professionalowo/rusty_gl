db59684532cfa545e19cbc88ca6ff86979c264b2
professionalowo <martin.brandl1307@gmail.com>
2025-08-26 16:41:43 +0200

    refactor: enhance try_load_opt function by improving type constraints and simplifying internal logic

--- src/framework/textures/stbi/load.rs
+++ src/framework/textures/stbi/load.rs
@@ -10,10 +10,45 @@ pub(super) fn try_load(bytes: &[u8]) -> Result<ImageData, ImageError> {
     try_load_opt::<LoadInt>(bytes)
 }
 
-fn try_load_opt<L: Load>(bytes: &[u8]) -> Result<ImageData, ImageError> {
+fn try_load_opt<L>(bytes: &[u8]) -> Result<ImageData, ImageError>
+where
+    L: Load,
+{
     unsafe {
         stbi_set_flip_vertically_on_load(1);
     }
+    let LoadData {
+        width,
+        height,
+        channels,
+        data,
+    } = load::<L>(bytes)?;
+    let internal_format = L::map_channels(channels);
+    let format = format_from_channels(channels);
+    Ok(ImageData {
+        width,
+        height,
+        format,
+        data,
+        type_: gl::GL_FLOAT,
+        internal_format: internal_format.try_into()?,
+    })
+}
+
+#[inline]
+fn format_from_channels(channels: i32) -> gl::GLenum {
+    match channels {
+        4 => gl::GL_RGBA,
+        3 => gl::GL_RGB,
+        2 => gl::GL_RG,
+        1 | _ => gl::GL_RED,
+    }
+}
+
+fn load<L>(bytes: &[u8]) -> Result<LoadData, ImageError>
+where
+    L: Load,
+{
     let mut width = 0;
     let mut height = 0;
     let mut channels = 0;
@@ -36,26 +71,12 @@ fn try_load_opt<L: Load>(bytes: &[u8]) -> Result<ImageData, ImageError> {
             (width * height * channels).try_into()?,
         ))
     };
-    let internal_format = L::map_channels(channels);
-    let format = format_from_channels(channels);
-    Ok(ImageData {
+    Ok(LoadData {
         width,
         height,
-        format,
+        channels,
         data,
-        type_: gl::GL_FLOAT,
-        internal_format: internal_format.try_into()?,
     })
-}
-
-fn format_from_channels(channels: i32) -> gl::GLenum {
-    match channels {
-        4 => gl::GL_RGBA,
-        3 => gl::GL_RGB,
-        2 => gl::GL_RG,
-        1 => gl::GL_RED,
-        _ => gl::GL_RED,
-    }
 }
 
 trait Load {
@@ -93,10 +114,8 @@ impl Load for LoadFloat {
         channels_in_file: *mut c_int,
         desired_channels: c_int,
     ) -> *mut u8 {
-        let ptr = unsafe {
-            stbi_loadf_from_memory(buffer, len, x, y, channels_in_file, desired_channels)
-        };
-        ptr.cast::<u8>()
+        unsafe { stbi_loadf_from_memory(buffer, len, x, y, channels_in_file, desired_channels) }
+            .cast::<u8>()
     }
 }
 
@@ -125,4 +144,11 @@ impl Load for LoadInt {
     ) -> *mut u8 {
         unsafe { stbi_load_from_memory(buffer, len, x, y, channels_in_file, desired_channels) }
     }
+}
+
+struct LoadData {
+    width: i32,
+    height: i32,
+    channels: i32,
+    data: Box<[u8]>,
 }
