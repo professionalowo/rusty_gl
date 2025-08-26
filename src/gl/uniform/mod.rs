ebf9f8f6c68b7ce787c6ea8f0c810a1609a82d02
professionalowo <martin.brandl1307@gmail.com>
2025-08-26 10:52:33 +0200

    refactor: rename program variable to id in UniformLocation methods for consistency

--- src/gl/uniform/mod.rs
+++ src/gl/uniform/mod.rs
@@ -19,14 +19,14 @@ impl UniformLocation {
         let name_cstr = ffi::CString::new(name)?;
         let name_ptr = name_cstr.as_ptr() as *const i8;
 
-        let Program(program) = *program;
-        let res = unsafe { gl::glGetUniformLocation(program, name_ptr) };
+        let Program(id) = *program;
+        let res = unsafe { gl::glGetUniformLocation(id, name_ptr) };
 
         gl::get_error()?;
 
         match res {
             -1 => Err(UniformLocationError::UnusedUniform {
-                program,
+                id,
                 name: String::from(name),
             }),
             loc => Ok(Self(loc)),
@@ -50,7 +50,7 @@ impl UniformLocation {
 
 #[derive(Debug)]
 pub enum UniformLocationError {
-    UnusedUniform { program: u32, name: String },
+    UnusedUniform { id: u32, name: String },
     FFIError(ffi::NulError),
     GlError(gl::GLError),
 }
@@ -58,8 +58,8 @@ pub enum UniformLocationError {
 impl fmt::Display for UniformLocationError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::UnusedUniform { name, program } => {
-                write!(f, "Uniform {} unused in program {}", name, program)
+            Self::UnusedUniform { name, id } => {
+                write!(f, "Uniform {} unused in program with id {}", name, id)
             }
             Self::FFIError(e) => fmt::Display::fmt(e, f),
             Self::GlError(e) => fmt::Display::fmt(e, f),
