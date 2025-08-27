672ca3f76bb0c507a90d90d5574e3698ad637081
professionalowo <martin.brandl1307@gmail.com>
2025-08-27 18:44:14 +0200

    refactor: make Material struct fields private

--- src/framework/material/mod.rs
+++ src/framework/material/mod.rs
@@ -6,9 +6,31 @@ use super::texture::Texture2D;
 
 #[derive(Debug)]
 pub struct Material {
-    pub textures: HashMap<String, Texture2D>,
-    pub k_amb: Vec4<f32>,
-    pub k_diff: Vec4<f32>,
-    pub k_spec: Vec4<f32>,
-    pub n_spec: f32,
+    textures: HashMap<String, Texture2D>,
+    k_amb: Vec4<f32>,
+    k_diff: Vec4<f32>,
+    k_spec: Vec4<f32>,
+    n_spec: f32,
+}
+
+impl Material {
+    pub fn add_texture(&mut self, key: String, value: Texture2D) -> Option<Texture2D> {
+        self.textures.insert(key, value)
+    }
+
+    pub const fn k_amb(&self) -> &Vec4<f32> {
+        &self.k_amb
+    }
+
+    pub const fn k_diff(&self) -> &Vec4<f32> {
+        &self.k_diff
+    }
+
+    pub const fn k_spec(&self) -> &Vec4<f32> {
+        &self.k_spec
+    }
+
+    pub const fn n_spec(&self) -> f32 {
+        self.n_spec
+    }
 }
