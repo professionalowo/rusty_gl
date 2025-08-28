b3d1627f6bab659df04ce90d7424e1ef79d7b566
professionalowo <martin.brandl1307@gmail.com>
2025-08-28 11:46:34 +0200

    feat: implement matrix inversion for Mat4 struct with manual calculation

--- src/math/mat4.rs
+++ src/math/mat4.rs
@@ -4,8 +4,6 @@ use std::{
 };
 
 const RANK: usize = 4;
-use ndarray::array;
-use ndarray_linalg::Inverse;
 
 use crate::{
     gl::{
@@ -90,51 +88,109 @@ impl Mat4<f32> {
     }
 
     pub fn invert(&self) -> Option<Self> {
-        self.inv().ok()
+        let mut inv = [0.0; 16];
+        let m: [f32; 16] = self.cols();
+
+        inv[0] = m[5] * m[10] * m[15] - m[5] * m[11] * m[14] - m[9] * m[6] * m[15]
+            + m[9] * m[7] * m[14]
+            + m[13] * m[6] * m[11]
+            - m[13] * m[7] * m[10];
+
+        inv[4] = -m[4] * m[10] * m[15] + m[4] * m[11] * m[14] + m[8] * m[6] * m[15]
+            - m[8] * m[7] * m[14]
+            - m[12] * m[6] * m[11]
+            + m[12] * m[7] * m[10];
+
+        inv[8] = m[4] * m[9] * m[15] - m[4] * m[11] * m[13] - m[8] * m[5] * m[15]
+            + m[8] * m[7] * m[13]
+            + m[12] * m[5] * m[11]
+            - m[12] * m[7] * m[9];
+
+        inv[12] = -m[4] * m[9] * m[14] + m[4] * m[10] * m[13] + m[8] * m[5] * m[14]
+            - m[8] * m[6] * m[13]
+            - m[12] * m[5] * m[10]
+            + m[12] * m[6] * m[9];
+
+        inv[1] = -m[1] * m[10] * m[15] + m[1] * m[11] * m[14] + m[9] * m[2] * m[15]
+            - m[9] * m[3] * m[14]
+            - m[13] * m[2] * m[11]
+            + m[13] * m[3] * m[10];
+
+        inv[5] = m[0] * m[10] * m[15] - m[0] * m[11] * m[14] - m[8] * m[2] * m[15]
+            + m[8] * m[3] * m[14]
+            + m[12] * m[2] * m[11]
+            - m[12] * m[3] * m[10];
+
+        inv[9] = -m[0] * m[9] * m[15] + m[0] * m[11] * m[13] + m[8] * m[1] * m[15]
+            - m[8] * m[3] * m[13]
+            - m[12] * m[1] * m[11]
+            + m[12] * m[3] * m[9];
+
+        inv[13] = m[0] * m[9] * m[14] - m[0] * m[10] * m[13] - m[8] * m[1] * m[14]
+            + m[8] * m[2] * m[13]
+            + m[12] * m[1] * m[10]
+            - m[12] * m[2] * m[9];
+
+        inv[2] = m[1] * m[6] * m[15] - m[1] * m[7] * m[14] - m[5] * m[2] * m[15]
+            + m[5] * m[3] * m[14]
+            + m[13] * m[2] * m[7]
+            - m[13] * m[3] * m[6];
+
+        inv[6] = -m[0] * m[6] * m[15] + m[0] * m[7] * m[14] + m[4] * m[2] * m[15]
+            - m[4] * m[3] * m[14]
+            - m[12] * m[2] * m[7]
+            + m[12] * m[3] * m[6];
+
+        inv[10] = m[0] * m[5] * m[15] - m[0] * m[7] * m[13] - m[4] * m[1] * m[15]
+            + m[4] * m[3] * m[13]
+            + m[12] * m[1] * m[7]
+            - m[12] * m[3] * m[5];
+
+        inv[14] = -m[0] * m[5] * m[14] + m[0] * m[6] * m[13] + m[4] * m[1] * m[14]
+            - m[4] * m[2] * m[13]
+            - m[12] * m[1] * m[6]
+            + m[12] * m[2] * m[5];
+
+        inv[3] = -m[1] * m[6] * m[11] + m[1] * m[7] * m[10] + m[5] * m[2] * m[11]
+            - m[5] * m[3] * m[10]
+            - m[9] * m[2] * m[7]
+            + m[9] * m[3] * m[6];
+
+        inv[7] = m[0] * m[6] * m[11] - m[0] * m[7] * m[10] - m[4] * m[2] * m[11]
+            + m[4] * m[3] * m[10]
+            + m[8] * m[2] * m[7]
+            - m[8] * m[3] * m[6];
+
+        inv[11] = -m[0] * m[5] * m[11] + m[0] * m[7] * m[9] + m[4] * m[1] * m[11]
+            - m[4] * m[3] * m[9]
+            - m[8] * m[1] * m[7]
+            + m[8] * m[3] * m[5];
+
+        inv[15] = m[0] * m[5] * m[10] - m[0] * m[6] * m[9] - m[4] * m[1] * m[10]
+            + m[4] * m[2] * m[9]
+            + m[8] * m[1] * m[6]
+            - m[8] * m[2] * m[5];
+
+        let mut det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+        if det == 0.0 {
+            return None;
+        }
+
+        det = 1.0 / det;
+
+        return Some(Self::from(inv.map(|x| x * det)));
     }
 }
 
-impl Inverse for Mat4<f32> {
-    type Output = Self;
-
-    fn inv(&self) -> ndarray_linalg::error::Result<Self::Output> {
-        let a = array![
-            [
-                self.cols[0].x,
-                self.cols[1].x,
-                self.cols[2].x,
-                self.cols[3].x
-            ],
-            [
-                self.cols[0].y,
-                self.cols[1].y,
-                self.cols[2].y,
-                self.cols[3].y
-            ],
-            [
-                self.cols[0].z,
-                self.cols[1].z,
-                self.cols[2].z,
-                self.cols[3].z
-            ],
-            [
-                self.cols[0].w,
-                self.cols[1].w,
-                self.cols[2].w,
-                self.cols[3].w
-            ],
-        ];
-        match a.inv() {
-            Err(e) => Err(e),
-            Ok(inv) => {
-                Ok(Self::new(
-                    Vec4::new(inv[(0, 0)], inv[(1, 0)], inv[(2, 0)], inv[(3, 0)]), // first column
-                    Vec4::new(inv[(0, 1)], inv[(1, 1)], inv[(2, 1)], inv[(3, 1)]), // second column
-                    Vec4::new(inv[(0, 2)], inv[(1, 2)], inv[(2, 2)], inv[(3, 2)]), // third column
-                    Vec4::new(inv[(0, 3)], inv[(1, 3)], inv[(2, 3)], inv[(3, 3)]), // fourth column
-                ))
-            }
-        }
+impl From<[f32; RANK * RANK]> for Mat4<f32> {
+    fn from(data: [f32; RANK * RANK]) -> Self {
+        Self::new(
+            Vec4::new(data[0], data[1], data[2], data[3]),
+            Vec4::new(data[4], data[5], data[6], data[7]),
+            Vec4::new(data[8], data[9], data[10], data[11]),
+            Vec4::new(data[12], data[13], data[14], data[15]),
+        )
     }
 }
 
