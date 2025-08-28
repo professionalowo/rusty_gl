7ff3ee5ad0dad2a7a38dce54ecd4a55d9d7afb69
professionalowo <martin.brandl1307@gmail.com>
2025-08-28 10:03:14 +0200

    refactor: simplify import for Vector3D in From trait implementation for Vec3<f32>

--- src/math/vec3/ai.rs
+++ src/math/vec3/ai.rs
@@ -1,7 +1,8 @@
 use super::Vec3;
+use assimp::Vector3D;
 
-impl From<assimp::Vector3D> for Vec3<f32> {
-    fn from(v: assimp::Vector3D) -> Self {
+impl From<Vector3D> for Vec3<f32> {
+    fn from(v: Vector3D) -> Self {
         Self {
             x: v.x,
             y: v.y,
