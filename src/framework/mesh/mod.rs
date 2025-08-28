d8fcf1e02e70ed6268ea3fa0d3740a0a053a83ff
professionalowo <martin.brandl1307@gmail.com>
2025-08-28 10:22:48 +0200

    feat: implement Mesh struct and MeshLoadError enum with loading functionality

--- src/framework/mesh/mod.rs
+++ src/framework/mesh/mod.rs
@@ -1,3 +1,36 @@
-pub struct Mesh {
-    
+use std::{fmt, path::PathBuf};
+
+pub struct Mesh {}
+
+#[derive(Debug)]
+pub enum MeshLoadError {
+    LoadFailed(String),
+    InvalidPath(PathBuf),
+}
+
+impl fmt::Display for MeshLoadError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MeshLoadError::LoadFailed(s) => write!(f, "Failed to load mesh: {}", s),
+            MeshLoadError::InvalidPath(p) => write!(f, "Invalid path: {:?}", p),
+        }
+    }
+}
+
+impl From<&str> for MeshLoadError {
+    fn from(s: &str) -> Self {
+        Self::LoadFailed(s.to_string())
+    }
+}
+
+pub fn load_mesh(path: PathBuf) -> Result<Mesh, MeshLoadError> {
+    let mut importer = assimp::Importer::new();
+    importer.triangulate(true);
+    importer.generate_normals(|_| ());
+
+    let path_str = path
+        .to_str()
+        .ok_or(MeshLoadError::InvalidPath(path.clone()))?;
+    let scene = importer.read_file(path_str)?;
+    Ok(Mesh {})
 }
