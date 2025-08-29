c9a1a7900cd24e854f748dd4a83ed9976a2bfb4b
professionalowo <martin.brandl1307@gmail.com>
2025-08-29 16:47:43 +0200

    refactor: change MaterialTextures::new to a const function for improved performance

--- src/framework/material/material_textures.rs
+++ src/framework/material/material_textures.rs
@@ -12,7 +12,7 @@ pub struct MaterialTextures {
 }
 
 impl MaterialTextures {
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self {
             diffuse: None,
             specular: None,
