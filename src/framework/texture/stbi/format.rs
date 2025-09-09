85e72da670ca2fa8121fd66f7ebaf8d9dcff54ef
professionalowo <martin.brandl1307@gmail.com>
2025-09-09 14:50:00 +0200

    refactor: move format logic into Channels struct for improved organization

--- src/framework/texture/stbi/format.rs
+++ src/framework/texture/stbi/format.rs
@@ -2,37 +2,38 @@ use super::{gl, load_trait::Load};
 use std::num::TryFromIntError;
 
 #[derive(Debug)]
+pub struct Channels(pub i32);
+
+impl Channels {
+    #[inline]
+    pub const fn format(&self) -> gl::GLenum {
+        match self.0 {
+            4 => gl::GL_RGBA,
+            3 => gl::GL_RGB,
+            2 => gl::GL_RG,
+            1 | _ => gl::GL_RED,
+        }
+    }
+}
+
+impl From<i32> for Channels {
+    fn from(value: i32) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Debug)]
 pub struct Format {
     pub format: u32,
     pub internal_format: i32,
 }
 
-#[derive(Debug)]
-pub struct Channels(pub i32);
-
-impl From<i32> for Channels {
-    fn from(value: i32) -> Self {
-        Channels(value)
-    }
-}
-
 impl Format {
     pub fn try_from_load<L: Load>(channels: &Channels) -> Result<Self, TryFromIntError> {
         let internal_format = i32::try_from(L::map_channels(channels))?;
-        let format = format_from_channels(channels);
         Ok(Self {
-            format,
+            format: channels.format(),
             internal_format,
         })
-    }
-}
-
-#[inline]
-const fn format_from_channels(Channels(channels): &Channels) -> gl::GLenum {
-    match channels {
-        4 => gl::GL_RGBA,
-        3 => gl::GL_RGB,
-        2 => gl::GL_RG,
-        1 | _ => gl::GL_RED,
     }
 }
