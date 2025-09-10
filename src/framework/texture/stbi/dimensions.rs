f2c8718bf7e32ec13ce7a198181158490c7e9d1e
professionalowo <martin.brandl1307@gmail.com>
2025-09-10 14:19:09 +0200

    refactor: add PartialEq derive to Dimensions struct and clean up code

--- src/framework/texture/stbi/dimensions.rs
+++ src/framework/texture/stbi/dimensions.rs
@@ -1,12 +1,16 @@
 use super::format::Channels;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Dimensions {
     pub width: i32,
     pub height: i32,
 }
 
 impl Dimensions {
+    pub const fn new(width: i32, height: i32) -> Self {
+        Self { width, height }
+    }
+
     pub const fn area(&self) -> i32 {
         self.width * self.height
     }
@@ -28,7 +32,6 @@ impl Into<(i32, i32)> for Dimensions {
     }
 }
 
-
 impl From<[i32; 2]> for Dimensions {
     fn from([width, height]: [i32; 2]) -> Self {
         Self { width, height }
@@ -38,5 +41,33 @@ impl From<[i32; 2]> for Dimensions {
 impl Into<[i32; 2]> for Dimensions {
     fn into(self) -> [i32; 2] {
         [self.width, self.height]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DIM: Dimensions = Dimensions::new(2, 3);
+
+    #[test]
+    fn test_dimensions_area() {
+        assert_eq!(6, DIM.area())
+    }
+
+    #[test]
+    fn test_dimensions_volume_with_channels() {
+        let ch = Channels(4);
+        assert_eq!(24, DIM.volume_with_channels(&ch))
+    }
+
+    #[test]
+    fn test_dimensions_from_tuple() {
+        assert_eq!(DIM, Dimensions::from((2, 3)))
+    }
+
+    #[test]
+    fn test_dimensions_from_array() {
+        assert_eq!(DIM, Dimensions::from([2, 3]))
     }
 }
