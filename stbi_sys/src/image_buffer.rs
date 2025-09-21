269d70bc9ff6cb2518248d2c14c673bc5086a1ee
professionalowo <martin.brandl1307@gmail.com>
2025-09-21 18:54:49 +0200

    merge

--- stbi_sys/src/image_buffer.rs
+++ stbi_sys/src/image_buffer.rs
@@ -34,13 +34,6 @@ impl ImageBuffer {
         }
     }
 
-    pub const fn null() -> Self {
-        Self {
-            ptr: std::ptr::null_mut(),
-            len: 0,
-        }
-    }
-
     pub const unsafe fn from_raw(ptr: *mut u8, len: usize) -> Self {
         //SAFETY: Caller must ensure that the pointer is valid and was allocated by stb_image
         //SAFETY: Caller must ensure that the pointer is valid and was allocated by stb_image
