bc39e640767e7049ece5af2404aa3eb5b4e796b6
professionalowo <martin.brandl1307@gmail.com>
2025-09-21 19:29:26 +0200

    refactor: add inline annotations to methods in StbImageBuffer for improved performance

--- stbi_sys/src/image_buffer.rs
+++ stbi_sys/src/image_buffer.rs
@@ -21,12 +21,14 @@ impl Drop for StbImageBuffer {
 }
 
 impl Default for StbImageBuffer {
+    #[inline]
     fn default() -> Self {
         Self::null()
     }
 }
 
 impl StbImageBuffer {
+    #[inline]
     pub const fn null() -> Self {
         Self {
             ptr: std::ptr::null_mut(),
@@ -34,35 +36,52 @@ impl StbImageBuffer {
         }
     }
 
+    #[inline]
     pub const unsafe fn from_raw_parts(ptr: *mut u8, len: usize) -> Self {
         //SAFETY: Caller must ensure that the pointer is valid and was allocated by stb_image
         Self { ptr, len }
     }
 
+    #[inline]
     pub const fn as_slice(&self) -> &[u8] {
         unsafe { slice::from_raw_parts(self.ptr, self.len) }
     }
 
+    #[inline]
     pub const fn as_mut_slice(&mut self) -> &mut [u8] {
         unsafe { slice::from_raw_parts_mut(self.ptr, self.len) }
     }
 
+    #[inline]
+    pub fn to_vec(self) -> Vec<u8> {
+        self.as_slice().to_vec()
+    }
+
+    #[inline]
+    pub const fn len(&self) -> usize {
+        self.len
+    }
+
+    #[inline]
     pub const fn is_null(&self) -> bool {
         self.ptr.is_null()
     }
 
-    pub fn to_vec(self) -> Vec<u8> {
-        self.as_slice().to_vec()
+    #[inline]
+    pub const fn is_empty(&self) -> bool {
+        self.len == 0
     }
 }
 
 impl AsRef<[u8]> for StbImageBuffer {
+    #[inline]
     fn as_ref(&self) -> &[u8] {
         self.as_slice()
     }
 }
 
 impl AsMut<[u8]> for StbImageBuffer {
+    #[inline]
     fn as_mut(&mut self) -> &mut [u8] {
         self.as_mut_slice()
     }
@@ -70,12 +89,15 @@ impl AsMut<[u8]> for StbImageBuffer {
 
 impl Deref for StbImageBuffer {
     type Target = [u8];
+
+    #[inline]
     fn deref(&self) -> &Self::Target {
         self.as_slice()
     }
 }
 
 impl DerefMut for StbImageBuffer {
+    #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.as_mut_slice()
     }
