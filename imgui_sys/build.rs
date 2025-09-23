74f01103a5337b0c3892d20a1c45b9f29fc00765
professionalowo <martin.brandl1307@gmail.com>
2025-09-23 11:38:20 +0200

    fix: move imgui source files into the build configuration

--- imgui_sys/build.rs
+++ imgui_sys/build.rs
@@ -3,6 +3,14 @@ use std::path::PathBuf;
 fn main() {
     cc::Build::new()
         .cpp(true)
+        .files([
+            "imgui/imgui.cpp",
+            "imgui/imgui_draw.cpp",
+            "imgui/imgui_tables.cpp",
+            "imgui/imgui_widgets.cpp",
+            "imgui/backends/imgui_impl_glfw.cpp",
+            "imgui/backends/imgui_impl_opengl3.cpp",
+        ])
         .file("imguiwrapper.cpp")
         .flags(["-Wno-unused-parameter", "-Wno-unused-function"])
         .include("imgui")
