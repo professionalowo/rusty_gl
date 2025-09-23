be0519889f31c0c3b4dad8eca0f6d088646513aa
professionalowo <martin.brandl1307@gmail.com>
2025-09-23 10:59:56 +0200

    feat: add imgui_sys module with initial C++ bindings and dependencies

---
+++ imgui_sys/build.rs
@@ -0,0 +1,28 @@
+use std::path::PathBuf;
+
+fn main() {
+    cc::Build::new()
+        .cpp(true)
+        .file("imguiwrapper.cpp")
+        .flags(["-Wno-unused-parameter", "-Wno-unused-function"])
+        .includes(["../imgui/", "../imgui/backends"])
+        .flag_if_supported("-std=c++17")
+        .try_compile("imgui")
+        .expect("Could not compile STBI header");
+
+    let bindings = bindgen::Builder::default()
+        .header("imguiwrapper.h")
+        .clang_arg("-x")
+        .clang_arg("c++")
+        .clang_arg("-std=c++17")
+        .clang_arg("-I../imgui")
+        .clang_arg("-I../imgui/backends")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("imgui_bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
