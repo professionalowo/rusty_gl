348c6e25bbc22d654d5a612e7a5805bdaf6e09a0
professionalowo <martin.brandl1307@gmail.com>
2025-09-27 19:24:01 +0200

    refactor: simplify path string conversion in load_ai_scene for improved clarity

--- app/src/framework/mesh/load.rs
+++ app/src/framework/mesh/load.rs
@@ -94,7 +94,7 @@ fn load_ai_scene<'a>(path: &Path) -> Result<assimp::Scene<'a>, MeshLoadError> {
     importer.triangulate(true);
     importer.generate_normals(|opt| opt.smooth = true);
 
-    let path_str = &path
+    let path_str = path
         .to_str()
         .ok_or_else(|| MeshLoadError::InvalidPath(path.to_path_buf()))?;
     let scene = importer.read_file(path_str)?;
